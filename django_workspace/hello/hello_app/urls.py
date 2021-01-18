ee88d87926d06e51e67642493939616afc50d8d4
kssgit <tjdtnmail@gmail.com>
2021-01-18 18:09:22 +0900

    django로 todo만들기

--- django_workspace/hello/hello_app/urls.py
+++ django_workspace/hello/hello_app/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # url , 파일 위치 , 함수 이름
+    # url(기본루트는 config 파일인 hello/urls.py에 적은 hello/이다), 컨트롤할 메서드 , 메서드 이름
+    path('', views.index, name='index'),
 ]
--- django_workspace/hello/hello_app/views.py
+++ django_workspace/hello/hello_app/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render  # HTML 렌더링하기 위한 모듈
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse  # HTTP 를 처리하기위한 모듈
 
 
 def index(request):
